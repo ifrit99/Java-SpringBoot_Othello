c6d1bb0100164d724af018a380472f58900932b4
ifrit99 <54010447+ifrit99@users.noreply.github.com>
2020-05-27 22:41:42 +0900

    インターフェースクラスを修正してOthelloServiceへ反転チェック処理書く途中

--- src/main/java/com/springbootdemo/OthelloService.java
+++ src/main/java/com/springbootdemo/OthelloService.java
@@ -1,12 +1,15 @@
 package com.springbootdemo;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public interface OthelloService {
-
-	// 反転対象探索用の移動処理
-	public HashMap<int, int, boolean> arrayMove();
+	// 戻り値で使用するMap
+	Map<String, Integer> resultMap = new LinkedHashMap<>();
 	
-	// 実際の処理定義用
-	void acutualProcessing();
+	// 反転対象探索用の移動処理（x=置いたX座標、ｙ＝置いたY座標、offsetX=オフセット値X、offsetY=オフセット値Y）
+	public Map<String,Integer> boadMove(int x, int y, int offsetX, int offsetY);
+	
+	// 実際の処理を行う抽象メソッド
+	public Map<String,Integer> acutualProcessing(int x, int y, Map<String,Integer> resultMap);
 }
--- src/main/java/com/springbootdemo/OthelloServiceImpl.java
+++ src/main/java/com/springbootdemo/OthelloServiceImpl.java
@@ -1,5 +1,34 @@
 package com.springbootdemo;
 
+import java.util.Map;
+
 public class OthelloServiceImpl implements OthelloService{
+
+	@Override
+	public Map<String, Integer> boadMove(int x, int y, int offsetX, int offsetY) {
+		// 盤上の間繰り返す
+		while (0 <= x && x < 8 && 0 <= y && y < 8) {
+			// x,yにオフセット値を適用
+			x += offsetX;
+			y += offsetY;
+			
+			// x,yが配列のインデックス範囲から出たらループ終了
+			if (x < 0 || y < 0 || x > 7 || y > 7) {
+				break;
+			}
+			
+			// 実際の処理を行うクラスの関数を呼び出し、返り値としてLinkedHashMapを受け取る
+			resultMap = acutualProcessing(x, y, resultMap);
+		}
+		
+		return resultMap;
+	}
+
+	@Override
+	public Map<String, Integer> acutualProcessing(int x, int y, Map<String, Integer> resultMap) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
 
 }
